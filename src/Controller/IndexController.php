c10fdb5b2a37b60fc36df5b4342e252bc825a095
Roland Golla <rolandgolla@gmail.com>
2019-11-30 04:49:07 +0100

    Add news servive and newsapi

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
+use App\Service\NewsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     /**
+     * @var NewsService
+     */
+    private $newsService;
+
+    public function __construct(NewsService $newsService)
+    {
+        $this->newsService = $newsService;
+    }
+
+    /**
      * @Route("/", name="index")
      */
     public function index()
     {
+        $news = $this->newsService->getNews();
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'news' => $news
         ]);
     }
 }
---
+++ src/Service/NewsService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpClient\HttpClient;
+
+class NewsService
+{
+    public function getNews()
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://newsapi.org/v2/everything?q=waterpolo&from=2019-10-30&sortBy=publishedAt&apiKey=' . $_ENV['NEWSAPI']);
+
+        $statusCode = $response->getStatusCode();
+        $contentType = $response->getHeaders()['content-type'][0];
+        $content = $response->getContent();
+        $content = $response->toArray();
+
+        return $content;
+    }
+}

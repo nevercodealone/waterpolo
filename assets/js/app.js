7aca269cbbdea5e96b42873f217c71dd3e0d3411
Roland Golla <rolandgolla@gmail.com>
2019-12-02 11:49:04 +0100

    add fornt awesome brands and add to base template nca github link

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,9 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 import 'bootstrap/dist/css/bootstrap.min.css';
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/css/brands.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it

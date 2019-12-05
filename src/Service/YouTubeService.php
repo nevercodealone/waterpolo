a9fdbd6638c6f02959c9d619d0d08191c3ce033d
Roland Golla <rolandgolla@gmail.com>
2019-12-05 01:20:41 +0100

    add cache for zoutube videos on YouTubeService.php

--- src/Service/YouTubeService.php
+++ src/Service/YouTubeService.php
@@ -1,14 +1,20 @@
 <?php
 namespace App\Service;
 
+use Psr\Cache\CacheItemPoolInterface;
+
 class YouTubeService
 {
     /** @var \Google_Service_YouTube */
     private $youtubeService;
 
-    public function __construct(\Google_Service_YouTube $youtubeService)
+    /** @var CacheItemPoolInterface */
+    private $cache;
+
+    public function __construct(\Google_Service_YouTube $youtubeService, CacheItemPoolInterface $cache)
     {
         $this->youtubeService = $youtubeService;
+        $this->cache = $cache;
     }
 
     public function getItemsFromChannel(): array
@@ -18,11 +24,19 @@ class YouTubeService
             'playlistId' => 'PLf9rhfhnyGJ-l6WlrMdy-0fZNcSEqjb4y'
         ];
 
-        $videoList = $this->playlistItemsListByPlaylistId('snippet',$params);
-        $videos = array_reverse($videoList['items']);
-        $videos = array_slice($videos, 0, 9);
+        $cacheItem = $this->cache->getItem('videos');
 
-        return $videos;
+        if (!$cacheItem->isHit()) {
+            $videoList = $this->playlistItemsListByPlaylistId('snippet',$params);
+            $videos = array_reverse($videoList['items']);
+            $videos = array_slice($videos, 0, 9);
+
+            $cacheItem->set($videos);
+            $this->cache->save($cacheItem);
+        }
+
+        return $cacheItem->get();
+
     }
 
     private function playlistItemsListByPlaylistId(string $part, array $params): \Google_Service_YouTube_PlaylistItemListResponse

a2c574d609847002ae7eef0289cd73b236b420f0
cdaschner <c.daschner@conversis.de>
2019-12-05 16:30:10 +0100

    generate google json file

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,16 @@ echo "GOOGLE_API_KEY=`cat $GOOGLE_API_KEY_FILE`" >> .env
 echo "GOOGLE_JSON=`cat $GOOGLE_JSON_FILE`" >> .env
 
 echo "{" >> `cat $GOOGLE_JSON_FILE`
-echo "\"type\"\: \"`cat $JSON_TYPE_FILE`\"," >> `cat $GOOGLE_JSON_FILE`
+echo "\"type\": \"`cat $JSON_TYPE_FILE`\"," >> `cat $GOOGLE_JSON_FILE`
+echo "\"project_id\": \"`cat $JSON_PROJECT_ID_FILE`\"," >> `cat $GOOGLE_JSON_FILE`
+echo "\"private_key_id\": \"`cat $JSON_PRIVATE_KEY_ID_FILE`\"," >> `cat $GOOGLE_JSON_FILE`
+echo "\"private_key\": \"`cat $JSON_PRIVATE_KEY_FILE`\"," >> `cat $GOOGLE_JSON_FILE`
+echo "\"client_email\": \"`cat $JSON_CLIENT_EMAIL_FILE`\"," >> `cat $GOOGLE_JSON_FILE`
+echo "\"client_id\": \"`cat $JSON_CLIENT_ID_FILE`\"," >> `cat $GOOGLE_JSON_FILE`
+echo "\"auth_uri\": \"`cat $JSON_AUTH_URI_FILE`\"," >> `cat $GOOGLE_JSON_FILE`
+echo "\"token_uri\": \"`cat $JSON_TOKEN_URI_FILE`\"," >> `cat $GOOGLE_JSON_FILE`
+echo "\"auth_provider_x509_cert_url\": \"`cat $JSON_AUTH_PROVIDER_X509_CERT_URL_FILE`\"," >> `cat $GOOGLE_JSON_FILE`
+echo "\"client_x509_cert_url\": \"`cat $JSON_CLIENT_X509_CERT_URL_FILE`\"" >> `cat $GOOGLE_JSON_FILE`
 echo "}" >> `cat $GOOGLE_JSON_FILE`
 
 exec "$@"

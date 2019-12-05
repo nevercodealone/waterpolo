90a59a2608d797503dd35e2d27aaf00563d7269a
cdaschner <c.daschner@conversis.de>
2019-12-05 15:34:08 +0100

    add secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "APP_ENV=`cat $APP_ENV_FILE`" >> .env
+echo "APP_SECRET=`cat $APP_SECRET_FILE`" >> .env
+echo "DATABASE_URL=`cat $DATABASE_URL_FILE`" >> .env
+echo "NEWSAPI=`cat $NEWSAPI_FILE`" >> .env
 echo "GOOGLE_API_KEY=`cat $GOOGLE_API_KEY_FILE`" >> .env
+echo "GOOGLE_JSON=`cat $GOOGLE_JSON_FILE`" >> .env
+
+echo "{" >> $GOOGLE_JSON_FILE
+echo "\"type\": \"`cat $JSON_TYPE_FILE\"," >> $GOOGLE_JSON_FILE
+echo "}" >> $GOOGLE_JSON_FILE
 
 exec "$@"

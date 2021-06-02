e30f45518ddcfc02562950a1fa76fecf32ca9fc5
Roland Golla <rolandgolla@gmail.com>
2021-06-03 00:42:23 +0200

    Use php cs fixes on IndexControllerTest.php

--- tests/Controller/IndexControllerTest.php
+++ tests/Controller/IndexControllerTest.php
@@ -1,20 +1,15 @@
 <?php
 
-
 namespace App\Tests\Controller;
-
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class IndexControllerTest extends WebTestCase
 {
-
-
-
     public function testStatusForRoutes()
     {
         $client = static::createClient([], [
-            'HTTP_HOST'       => '127.0.0.1:8000'
+            'HTTP_HOST' => '127.0.0.1:8000',
         ]);
 
         $client->request('GET', '/videos/');
@@ -25,7 +20,7 @@ class IndexControllerTest extends WebTestCase
     public function testNewsHeadline()
     {
         $client = static::createClient([], [
-            'HTTP_HOST'       => '127.0.0.1:8000'
+            'HTTP_HOST' => '127.0.0.1:8000',
         ]);
 
         $client->request('GET', '/');
@@ -36,16 +31,14 @@ class IndexControllerTest extends WebTestCase
     public function testNewsGreaterTwo()
     {
         $client = static::createClient([], [
-            'HTTP_HOST'       => '127.0.0.1:8000'
+            'HTTP_HOST' => '127.0.0.1:8000',
         ]);
 
         $crawler = $client->request('GET', '/');
-
 
         $this->assertGreaterThan(
             2,
             $crawler->filter('h2')->count()
         );
     }
-
 }

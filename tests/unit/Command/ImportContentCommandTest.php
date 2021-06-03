0c7a2904c134d17e37f3d5c04b1deb48f22aee87
Roland Golla <rolandgolla@gmail.com>
2021-06-03 23:28:18 +0200

    Use mocks in IndexControllerTest.php

--- tests/unit/Command/ImportContentCommandTest.php
+++ tests/unit/Command/ImportContentCommandTest.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Command;
 
+use App\Command\ImportContentCommand;
+use App\Service\GrabberService;
+use App\Service\NewsService;
+use App\Service\YouTubeService;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -12,8 +18,39 @@ final class ImportContentCommandTest extends KernelTestCase
 {
     public function testExecute()
     {
-        $kernel = static::createKernel();
+        $kernel = self::bootKernel();
+
         $application = new Application($kernel);
+
+        $youTubeService = $this->createMock(YouTubeService::class);
+        $youTubeService->expects($this->once())
+            ->method('getVideoByKeywordsFromApi')
+            ->willReturn([
+                'videos' => [],
+            ]);
+
+        $newsService = $this->createMock(NewsService::class);
+        $itemMock = $this->createMock(CacheItemInterface::class);
+        $itemMock->expects($this->once())
+            ->method('set')
+            ->willReturn(true);
+        $cacheItemPoolInterface = $this->createMock(CacheItemPoolInterface::class);
+        $cacheItemPoolInterface->expects($this->once())
+            ->method('getItem')
+            ->willReturn($itemMock);
+        $grabberService = $this->createMock(GrabberService::class);
+        $grabberService->expects($this->once())
+            ->method('getItems')
+            ->willReturn([
+                'news' => [],
+                'sourceDomains' => [],
+                ]);
+        $application->add(new ImportContentCommand(
+            $youTubeService,
+            $newsService,
+            $cacheItemPoolInterface,
+            $grabberService
+        ));
 
         $command = $application->find('app:import:content');
         $commandTester = new CommandTester($command);

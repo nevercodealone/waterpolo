92245f32c720d590d50eb4495502c88b1711a723
Roland Golla <rolandgolla@gmail.com>
2021-06-03 02:01:21 +0200

    php cs fixer

--- tests/unit/Command/ImportContentCommandTest.php
+++ tests/unit/Command/ImportContentCommandTest.php
@@ -4,16 +4,9 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Command;
 
-
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
-use App\Command\ImportContentCommand;
-use App\Service\GrabberService;
-use App\Service\NewsService;
-use App\Service\YouTubeService;
-use PHPUnit\Framework\TestCase;
-use Psr\Cache\CacheItemPoolInterface;
 
 final class ImportContentCommandTest extends KernelTestCase
 {
@@ -25,13 +18,10 @@ final class ImportContentCommandTest extends KernelTestCase
         $command = $application->find('app:import:content');
         $commandTester = new CommandTester($command);
         $commandTester->execute([
-            
         ]);
 
         // the output of the command in the console
         $output = $commandTester->getDisplay();
         $this->assertStringContainsString('End job.', $output);
-
-        // ...
     }
 }

66cc2fbb4879b12a74a50ecd9e065057be0d8e95
Roland Golla <rolandgolla@gmail.com>
2021-06-13 11:46:27 +0200

    Try absolute path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,13 @@
 <?php
+define('BASE_PATH', realpath(dirname(__DIR__)));
+$_SERVER['DOCUMENT_ROOT'] = BASE_PATH;
+
+use Symfony\Component\Dotenv\Dotenv;
 
 require '/var/www/html/vendor/autoload.php';
 
-require '/var/www/html/config/bootstrap.php';
+if (file_exists('/var/www/html//config/bootstrap.php')) {
+    require '/var/www/html/config/bootstrap.php';
+} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}

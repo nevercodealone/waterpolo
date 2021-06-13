2e021e22d9aa86d7bfe4b55878fc13e5185b74d0
Roland Golla <rolandgolla@gmail.com>
2021-06-13 13:01:18 +0200

    Use boostrap rancher file for test oipeline

---
+++ tests/bootstrap-rancher.php
@@ -0,0 +1,15 @@
+<?php
+define('BASE_PATH', realpath(dirname(__DIR__)));
+$_SERVER['DOCUMENT_ROOT'] = BASE_PATH;
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require '/var/www/html/vendor/autoload.php';
+
+if (file_exists('/var/www/html//config/bootstrap.php')) {
+    require '/var/www/html/config/bootstrap.php';
+} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+    if(file_exists(dirname(__DIR__).'/.env')) {
+        (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    }
+}
